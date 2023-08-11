fe4fcd7c3373a18dfe6f5c9930d2e31b8d1e7263
FabricioHidalgo <136138454+FabricioHidalgo@users.noreply.github.com>
2023-08-10 20:55:45 -0600

    added javascript logic per season

---
+++ javascript.js
@@ -0,0 +1,13 @@
+
+
+let fecha=new Date();
+let mes=fecha.getMonth()+1;
+let verano=[12,1,2,3,4];
+let invierno=[5,6,7,8,9,10,11];
+
+//mes=1
+if (verano.indexOf(mes)==-1){
+    $('#verano').hide()
+}else{
+    $('#invierno').hide()
+}
